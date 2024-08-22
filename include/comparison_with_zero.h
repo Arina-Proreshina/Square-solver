c8d923bac50fabe2dd4ebc1be467eb8e2fbe0820
Arina Proreshina <proreshina.a@gmail.com>
2024-08-22 23:33:24 +0300

    Add files via upload

---
+++ include/comparison_with_zero.h
@@ -0,0 +1,10 @@
+#ifndef COMPARISON_WITH_ZERO_H_INCLUDED
+#define COMPARISON_WITH_ZERO_H_INCLUDED
+
+const double EPSILON = 1e-6;
+
+bool is_close(double x, double y);
+
+bool is_zero(double value);
+
+#endif // COMPARISON_WITH_ZERO_H_INCLUDED
